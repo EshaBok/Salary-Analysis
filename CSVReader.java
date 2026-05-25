655e8461a3b48d6e24ef633ab714857976a7253f
EshaBok <eshabokhari@outlook.com>
2026-05-25 14:58:04 -0700

    Add files via upload

---
+++ CSVReader.java
@@ -0,0 +1,33 @@
+import java.io.*;
+
+public class CSVReader {
+    public static void main (String[] args) {
+        String file = "SRC\\Department_Salaries_File.csv";
+        BufferedReader reader = null;
+        String line = "";
+
+        try {
+            reader = new BufferedReader(new FileReader(file));
+            while((line = reader.readLine()) != null) {
+                
+                String[] row = line.split(",");
+
+                for(String index : row) {
+                    System.out.printf("%-10s", index);
+                }
+                System.out.println();
+            }
+        }
+        catch(Exception e) {
+            e.printStackTrace();
+
+        }
+        finally {
+            try {
+                reader.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+}
